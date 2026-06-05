49400ebcf4376a3f319bd0f02942581385e77e0e
Chaitali Karmakar <chaitali.karmakar.90@gmail.com>
2026-06-05 09:16:59 -0700

    UI changes

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function showMessage() {
-    alert("Welcome Students! Welcome to the Github branching!!!");
+    alert("Hello Students! Welcome to GitHub Branching");
+}
 
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let message = document.getElementById("message");
+
+    if (username === "admin" && password === "1234") {
+        message.style.color = "green";
+        message.innerText = "Login Successful!";
+    } else {
+        message.style.color = "red";
+        message.innerText = "Invalid Credentials!";
+    }
 }
